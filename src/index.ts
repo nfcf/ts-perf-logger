b7d38b68cc177e6a971b8f1d17c14014168bf8cb
Nuno Freire <nunofcf@gmail.com>
2017-07-12 10:30:50 +0100

    attempt at making the non-built version to be used as dependency directly from git

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 export * from './interfaces';
-// export * from './default-options';
 export * from './perflog';
 export * from './perflog-manager';
 export * from './perflog-decorators';
-// export { ClassLogger, ClassLoggerWithoutArgs } from './class-logger.decorator';
-// export { Logger, LoggerWithoutArgs, DisableMethodLogger } from './function-logger.decorator';
