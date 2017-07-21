f8d5c22fef4900b0d0ae606605e1fe71a6513411
Nuno Freire <nunofcf@gmail.com>
2017-07-21 14:18:01 +0100

    couldn't get this to work to export / import in another project correctly...

---
+++ src/perflog-rxjs-operators.ts
@@ -0,0 +1,35 @@
+/*
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import { PerfLogManager } from './index';
+
+export function logPerf<T>(this: Observable<T>, key: string, actionId?: any): Observable<any> {
+  let source = this;
+
+  return Observable.create((observer: Observer<T>) => {
+    PerfLogManager.logPerfInit(key, actionId);
+    return source.subscribe(
+      (x: any) => {
+        observer.next(x);
+      },
+      (error: any) => {
+        PerfLogManager.logPerfEnd(key, false);
+        observer.error(error);
+      },
+      () => {
+        PerfLogManager.logPerfEnd(key, true);
+        observer.complete();
+      }
+    );
+  })
+}
+
+Observable.prototype.logPerf = logPerf;
+*/
+
+/*tslint:disable:no-shadowed-variable*/
+/*declare module 'rxjs/Observable' {
+  interface Observable<T> {
+    logPerf: typeof logPerf;
+  }
+}*/
